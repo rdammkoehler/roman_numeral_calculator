e4ac800829c9cd2e251c9beab9e84b6e369edd2a
Rich Dammkoehler <rpd@noradltd.com>
2016-08-23 22:07:41 +0000

    one simple test, make sure our wiring is good

---
+++ src/calculator.c
@@ -0,0 +1,8 @@
+#include "calculator.h"
+#include <string.h>
+
+void add(char *result, const char *left_oper, const char *right_oper)
+{
+	strcpy(result, "II");
+}
+
---
+++ src/calculator.h
@@ -0,0 +1,6 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+void add(char *result, const char *left_opr, const char *right_opr);
+
+#endif
