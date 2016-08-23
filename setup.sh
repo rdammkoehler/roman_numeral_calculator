f1c66c93b1e4e813e330bc1bc85462f22e94469e
Rich Dammkoehler <rpd@noradltd.com>
2016-08-23 21:28:06 +0000

    a setup script for the root user to use, this will setup our environment and build/install check for us

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# only allow root to run this script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo 'installing development toolchains'
+apt-get install -y automake autoconf libtool pkg-config texinfo gcc make splint vim
+
+echo 'building check-0.10.0 from source'
+cd check-0.10.0
+autoreconf --install
+./configure && make && make install
+
