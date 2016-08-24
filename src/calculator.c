3b686adb66e940f1409c13f244ebae9aaa2bb304
Rich Dammkoehler <rpd@noradltd.com>
2016-08-24 04:02:28 +0000

    expand CD to CCCC for addition

--- src/calculator.c
+++ src/calculator.c
@@ -41,6 +41,7 @@ void contract(char *result)
 void expand(char *operand, const char *oper)
 {
 	strcpy(operand, oper);
+	replace(operand, "CD", "CCCC");
 	replace(operand, "XC", "LXXXX");
 	replace(operand, "XL", "XXXX");
 	replace(operand, "IX", "VIIII");
