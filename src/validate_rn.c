6d11abb1b79a34074168caaae012315b9a107592
Rich Dammkoehler <rpd@noradltd.com>
2016-09-09 01:31:26 +0000

    flipped the if, now compress

--- src/validate_rn.c
+++ src/validate_rn.c
@@ -8,14 +8,9 @@ bool valid_rn(const char *str)
 	bool valid = false;
 	const char *pattern = "^M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$";
 	regex_t regex;
-	int regexec_ = 0;
 	if (0 == regcomp(&regex, pattern, REG_EXTENDED + REG_NOSUB))
 	{
-		regexec_ = regexec(&regex, str, 0, NULL, 0);
-		if (REG_NOMATCH == regexec_ || REG_ESPACE == regexec_)
-		{
-		}
-		else
+		if (0 == regexec(&regex, str, 0, NULL, 0))
 		{
 			valid = true;
 		}
