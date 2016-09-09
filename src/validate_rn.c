2369ba3efcf615343ac5f0c51b7694f0804cf9eb
Rich Dammkoehler <rpd@noradltd.com>
2016-09-09 00:39:33 +0000

    no xxl

--- src/validate_rn.c
+++ src/validate_rn.c
@@ -5,7 +5,7 @@
 
 bool valid_rn(const char *str)
 {
-	const char *pattern = "^M{0,3}((CM|CD){0,1}|C{0,3})X{0,2}L?$";
+	const char *pattern = "^M{0,3}((CM|CD){0,1}|C{0,3})(X?L?|X{0,2})$";
 	regex_t regex;
 	int regexec_ = 0;
 	printf("valid_rn ? %s\n", str);
