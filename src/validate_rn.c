244ba647e533d48153203dd89ab9bc2686b52e34
Rich Dammkoehler <rpd@noradltd.com>
2016-09-09 00:58:10 +0000

    add some exhaustive positives to ensure we have covered the bases, also remove output from the function (I think it works and we wont need this anymore)

--- src/validate_rn.c
+++ src/validate_rn.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "validate_rn.h"
 #include <regex.h>
@@ -8,20 +8,13 @@ bool valid_rn(const char *str)
 	const char *pattern = "^M{0,3}((CM|CD){0,1}|C{0,3})(X?L?|X{0,3})I?V?I{0,2}$";
 	regex_t regex;
 	int regexec_ = 0;
-	printf("valid_rn ? %s\n", str);
 	if (0 == regcomp(&regex, pattern, REG_EXTENDED + REG_NOSUB)) //don't need to validate after we know it works
 	{
 		regexec_ = regexec(&regex, str, 0, NULL, 0);
 		if (REG_NOMATCH == regexec_ || REG_ESPACE == regexec_)
 		{
-			printf("%s doesn't match %s\n", str, pattern);
 			return false;
 		}
-		printf("%s matches %s\n", str, pattern);
-	}
-	else
-	{
-		puts("regcomp failed");
 	}
 	regfree(&regex);
 	return true;
