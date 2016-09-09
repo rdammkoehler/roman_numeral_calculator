0e6e856c37a3849d6718867a28071cb2f9ba1301
Rich Dammkoehler <rpd@noradltd.com>
2016-09-09 00:11:05 +0000

    some very basic regexing, more to do

--- src/validate_rn.c
+++ src/validate_rn.c
@@ -1,9 +1,28 @@
+#include <stdio.h>
 #include <stdbool.h>
 #include "validate_rn.h"
+#include <regex.h>
 
 bool valid_rn(const char *str)
 {
-
-	return false;
+	const char *pattern = "^M{0,3}$";
+	regex_t regex;
+	int regexec_ = 0;
+	printf("valid_rn ? %s\n", str);
+	if (0 == regcomp(&regex, pattern, REG_EXTENDED + REG_NOSUB)) //don't need to validate after we know it works
+	{
+		regexec_ = regexec(&regex, str, 0, NULL, 0);
+		if (REG_NOMATCH == regexec_ || REG_ESPACE == regexec_)
+		{
+			printf("%s doesn't match %s\n", str, pattern);
+			return false;
+		}
+		printf("%s matches %s\n", str, pattern);
+	}
+	else
+	{
+		puts("regcomp failed");
+	}
+	regfree(&regex);
+	return true;
 }
-
