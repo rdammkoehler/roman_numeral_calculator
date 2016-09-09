0fbd471b967461ea7a40540cbc40d3838bea0f40
Rich Dammkoehler <rpd@noradltd.com>
2016-09-09 00:30:51 +0000

    allow l after cm

--- src/validate_rn.c
+++ src/validate_rn.c
@@ -5,7 +5,7 @@
 
 bool valid_rn(const char *str)
 {
-	const char *pattern = "^M{0,3}((CM|CD){0,1}|C{0,3})$";
+	const char *pattern = "^M{0,3}((CM|CD){0,1}|C{0,3})L?$";
 	regex_t regex;
 	int regexec_ = 0;
 	printf("valid_rn ? %s\n", str);
