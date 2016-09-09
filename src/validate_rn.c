96c256206d91ee08699458cb8ba85de6847bd2cf
Rich Dammkoehler <rpd@noradltd.com>
2016-09-09 01:30:27 +0000

    prep for refactor step

--- src/validate_rn.c
+++ src/validate_rn.c
@@ -5,16 +5,19 @@
 
 bool valid_rn(const char *str)
 {
-	bool valid = true;
+	bool valid = false;
 	const char *pattern = "^M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$";
 	regex_t regex;
 	int regexec_ = 0;
-	if (0 == regcomp(&regex, pattern, REG_EXTENDED + REG_NOSUB)) //don't need to validate after we know it works
+	if (0 == regcomp(&regex, pattern, REG_EXTENDED + REG_NOSUB))
 	{
 		regexec_ = regexec(&regex, str, 0, NULL, 0);
 		if (REG_NOMATCH == regexec_ || REG_ESPACE == regexec_)
 		{
-			valid = false;
+		}
+		else
+		{
+			valid = true;
 		}
 	}
 	regfree(&regex);
